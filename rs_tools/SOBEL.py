bae4bcfbcfaa073955f88b5a05754347b63748b2
“sangwonl” <sangwonl@uvic.ca>
2020-07-18 23:48:22 -0700

    SOBEL filters from 4 aspects left, right, top, bottom

--- rs_tools/SOBEL.py
+++ rs_tools/SOBEL.py
@@ -15,16 +15,15 @@ def generate_SOBEL(inFile):
         exit(1)
 
     image = pix_val_list(inImage, RGB=False)
-    SOBEL_horizontal = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]
-    SOBEL_vertical = [[1, 2, 1], [0, 0, 0], [-1, -2, -1]]
+    SOBEL_horizontal_left = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]
+    SOBEL_horizontal_right = [[1, 0, -1], [2, 0, -2], [1, 0, -1]]
+    SOBEL_vertical_bottom = [[1, 2, 1], [0, 0, 0], [-1, -2, -1]]
+    SOBEL_vertical_top = [[-1, -2, -1], [0, 0, 0], [1, 2, 1]]
 
-    horizontal_Gaussian = convolution(image, SOBEL_horizontal)
-    vertical_Gaussian = convolution(image, SOBEL_vertical)
-
-    output_name = "horizontal"
-    output_to_window(output_name, horizontal_Gaussian)
-    output_name = "vertical"
-    output_to_window(output_name, vertical_Gaussian)
+    horizontal_Gaussian_left = convolution(image, SOBEL_horizontal_left)
+    horizontal_Gaussian_right = convolution(image, SOBEL_horizontal_right)
+    vertical_Gaussian_bottom = convolution(image, SOBEL_vertical_bottom)
+    vertical_Gaussian_top = convolution(image, SOBEL_vertical_top)
 
     num_rows = len(image)
     num_cols = len(image[0])
@@ -33,7 +32,10 @@ def generate_SOBEL(inFile):
     for row in range(num_rows):
         for col in range(num_cols):
             SOBEL_Gaussian[row][col] = (
-                horizontal_Gaussian[row][col] + vertical_Gaussian[row][col]
+                horizontal_Gaussian_left[row][col]
+                + horizontal_Gaussian_left[row][col]
+                + vertical_Gaussian_bottom[row][col]
+                + vertical_Gaussian_top[row][col]
             )
 
     return SOBEL_Gaussian
