e3afc08c5070939b3e6241a2ef125c8bc46cd0ef
“sangwonl” <sangwonl@uvic.ca>
2020-07-18 23:21:44 -0700

    SOBEL outputs to a file

--- rs_tools/SOBEL.py
+++ rs_tools/SOBEL.py
@@ -1,10 +1,9 @@
 from rs_tools.utils import install, pix_val_list, convolution, output_to_window
-import sys
-import re
-import os
 
 install()
+
 import numpy
+import sys
 from PIL import Image, ImageFile, ImageDraw
 
 
@@ -35,6 +34,6 @@ def generate_SOBEL(inFile):
         for col in range(num_cols):
             SOBEL_Gaussian[row][col] = (
                 horizontal_Gaussian[row][col] + vertical_Gaussian[row][col]
-            ) / 2
+            )
 
     return SOBEL_Gaussian
